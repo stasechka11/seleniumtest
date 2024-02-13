4b255eabbd089e84877fb7a331b94e85011a0720
Anastasia Fokina <45092650+stasechka11@users.noreply.github.com>
2024-02-13 15:30:34 +0400

    added shouldSendRequestCardSuccess test

---
+++ src/test/java/ru/netology/web/CardRequestTest.java
@@ -0,0 +1,56 @@
+package ru.netology.web;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CardRequestTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999/");
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    public void shouldSendRequestCardSuccess() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Петров Алексей");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79271232457");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+
+        WebElement actualTextElement = driver.findElement(By.cssSelector("[data-test-id='order-success']"));
+        String actualText = actualTextElement.getText().trim();
+        String expectedText = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+
+        assertTrue(actualTextElement.isDisplayed());
+        assertEquals(expectedText, actualText);
+
+    }
+}
